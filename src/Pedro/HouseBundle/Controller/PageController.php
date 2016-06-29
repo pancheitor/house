e344c42adf6e79e457ebb3697515de33046e98a3
root <root@precise32.(none)>
2016-06-29 11:36:32 +0000

    paginas de modulos creadas

--- src/Pedro/HouseBundle/Controller/PageController.php
+++ src/Pedro/HouseBundle/Controller/PageController.php
@@ -10,4 +10,26 @@ class PageController extends Controller
     {
         return $this->render('PedroHouseBundle:Page:index.html.twig');
     }
+    public function huertoAction()
+    {
+        return $this->render('PedroHouseBundle:Page:huerto.html.twig');
+    }
+    public function casaAction()
+    {
+        return $this->render('PedroHouseBundle:Page:casa.html.twig');
+    }
+    public function aguasAction()
+    {
+        return $this->render('PedroHouseBundle:Page:aguas.html.twig');
+    }
+    public function energiaAction()
+    {
+        return $this->render('PedroHouseBundle:Page:energia.html.twig');
+    }
+    public function configuracionAction()
+    {
+        return $this->render('PedroHouseBundle:Page:configuracion.html.twig');
+    }
+
+    
 }
